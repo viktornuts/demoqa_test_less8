158828c8176b4f543f36542096f601b38bc75ba6
viktornuts <viktornuts@gmail.com>
2022-01-20 14:28:47 +0500

    Finish HW

--- src/test/java/guru/qa/HomeWork.java
+++ src/test/java/guru/qa/HomeWork.java
@@ -4,8 +4,6 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -34,7 +32,7 @@ public class HomeWork {
 
     @MethodSource("commonSearchTestCsvSource")
     @ParameterizedTest(name = "Тест проверят работу сервиса по определеню оператора по номеру телефона с данными: {0}")
-    void numberTest (String testData, String expected) {
+    void numberTest(String testData, String expected) {
         Selenide.open("https://www.spravportal.ru/Services/PhoneCodes/MobilePhoneInfo.aspx");
         $("input[name = 'ctl00$ctl00$cphMain$cphServiceMain$textNum']").setValue(testData);
         $("input[type='submit']").click();
@@ -48,16 +46,12 @@ public class HomeWork {
             "89049764432, TELE2"
     })
     @ParameterizedTest(name = "Тест проверят работу сервиса по определеню оператора по номеру телефона с данными: {0}")
-    void numberTest1 (String testData, String expected) {
+    void numberTest1(String testData, String expected) {
         Selenide.open("https://www.spravportal.ru/Services/PhoneCodes/MobilePhoneInfo.aspx");
         $("input[name = 'ctl00$ctl00$cphMain$cphServiceMain$textNum']").setValue(testData);
         $("input[type='submit']").click();
         $("#lblOp").shouldHave(Condition.text(expected));
     }
-
-
-
-
 
 
     // Сервис по определению оператора по номеру телефона отрабатывает корректно
